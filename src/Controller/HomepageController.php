f3fb7a17ae4d6f3166aa494182d7e3056558c88c
Dale.Nash <dalenashdn@gmail.com>
2021-05-21 16:26:06 +0100

    Add homepage!

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,85 +2,20 @@
 
 namespace App\Controller;
 
-use App\Entity\UserPypPost;
-use Doctrine\Common\Collections\Criteria;
-use Doctrine\Common\Collections\Expr\Comparison;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     /**
-     * @Route("/", name="app_index")
-     *
-     * @param EntityManagerInterface $em
-     *
-     * @return Response
+     * @Route("/home", name="app_home")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index()
     {
-        $currentUser = $this->getUser();
-
-        $userPypPosts = $em->getRepository(UserPypPost::class)->findBy(['user' => $currentUser], ['post' => 'DESC']);
-
-        foreach ($userPypPosts as $postKey => $userPypPost) {
-            $reportedComments = [];
-            $commentCount = $likeCount = 0;
-            $liked = false;
-
-            $interactions = $userPypPost->getPost()->getInteractions();
-
-            foreach ($interactions as $interaction) {
-                if ('comment' === $interaction->getType()) {
-                    ++$commentCount;
-
-                    if ($interaction->getReports()->matching(
-                        (new Criteria())->where(
-                            new Comparison('user', '=', $currentUser)
-                        ))
-                    ) {
-                        $reportedComments[] = $interaction;
-                    }
-                } else {
-                    if ($interaction->getUser() === $currentUser) {
-                        $liked = true;
-                    }
-                    ++$likeCount;
-                }
-            }
-
-            $userPypPosts[$postKey] = [
-                'post' => $userPypPost->getPost(),
-                'user' => $userPypPost->getPost()->getUser(),
-                'commentCount' => $commentCount,
-                'likeCount' => $likeCount,
-                'liked' => $liked,
-                'currentUser' => $currentUser,
-                'interactions' => $interactions,
-                'reportedComments' => $reportedComments,
-            ];
+        if (null !== $this->getUser()) {
+            return $this->redirectToRoute('app_index');
         }
 
-        return $this->render('homepage/index.html.twig', [
-            'posts' => $userPypPosts,
-        ]);
-    }
-
-    /**
-     * @Route("/privacy", name="app_privacy_policy")
-     */
-    public function privacyPolicy(): Response
-    {
-        return $this->render('homepage/privacy.html.twig');
-    }
-
-    /**
-     * @Route("/sw.js", name="sw_js_route")
-     */
-    public function swJsRoute(): Response
-    {
-        return new Response();
+        return $this->render('homepage/index.html.twig', ['isHome' => true]);
     }
 }
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\UserPypPost;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Expr\Comparison;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     *
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function index(EntityManagerInterface $em): Response
+    {
+        $currentUser = $this->getUser();
+
+        if (null === $currentUser) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $userPypPosts = $em->getRepository(UserPypPost::class)->findBy(['user' => $currentUser], ['post' => 'DESC']);
+
+        foreach ($userPypPosts as $postKey => $userPypPost) {
+            $reportedComments = [];
+            $commentCount = $likeCount = 0;
+            $liked = false;
+
+            $interactions = $this->getInteractionsData($userPypPost, $currentUser, $commentCount, $likeCount, $reportedComments, $liked);
+
+            $userPypPosts[$postKey] = [
+                'post' => $userPypPost->getPost(),
+                'user' => $userPypPost->getPost()->getUser(),
+                'commentCount' => $commentCount,
+                'likeCount' => $likeCount,
+                'liked' => $liked,
+                'currentUser' => $currentUser,
+                'interactions' => $interactions,
+                'reportedComments' => $reportedComments,
+            ];
+        }
+
+        return $this->render('index/index.html.twig', [
+            'posts' => $userPypPosts,
+        ]);
+    }
+
+    /**
+     * @Route("/privacy", name="app_privacy_policy")
+     */
+    public function privacyPolicy(): Response
+    {
+        return $this->render('index/privacy.html.twig');
+    }
+
+    /**
+     * @Route("/sw.js", name="sw_js_route")
+     */
+    public function swJsRoute(): Response
+    {
+        return new Response();
+    }
+
+    private function getInteractionsData(
+        UserPypPost $userPypPost,
+        UserInterface $currentUser,
+        int &$commentCount,
+        int &$likeCount,
+        array &$reportedComments,
+        bool &$liked
+    ): Collection {
+        $interactions = $userPypPost->getPost()->getInteractions();
+
+        foreach ($interactions as $interaction) {
+            if ('comment' === $interaction->getType()) {
+                ++$commentCount;
+
+                if ($interaction->getReports()->matching(
+                    (new Criteria())->where(
+                        new Comparison('user', '=', $currentUser)
+                    ))
+                ) {
+                    $reportedComments[] = $interaction;
+                }
+            } else {
+                if ($interaction->getUser() === $currentUser) {
+                    $liked = true;
+                }
+                ++$likeCount;
+            }
+        }
+
+        return $interactions;
+    }
+}
