d4bb5e65f59c005057ac2c262a4ea06182b1e218
unknown <massey.h0@gmail.com>
2021-08-23 01:37:00 +0100

    I have set the length of a mobile number in the user entity.
    One thing I did think is that although the js stops any non-numerical input, it might be good to handle for spaces somewhere by stripping it from the input, but I don't know where that would be best.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,7 +32,7 @@ class User implements UserInterface
     private $email;
  
     /**
-     * @ORM\Column(type="string", length=180)
+     * @ORM\Column(type="string", length=13)
      */
     private $mobile;
     
